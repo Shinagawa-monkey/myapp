81d6f6d7c9200865c5b2a3a1962353ea565dd46e
Shinagawa-monkey <helena.shatalova.art@gmail.com>
2022-05-13 20:59:44 -0500

    Implement /leads route
    Show a collection of leads upon submission

--- controllers/landing.js
+++ controllers/landing.js
@@ -8,6 +8,12 @@ exports.submit_lead = ((req, res, next) => {
   return models.Lead.create({
     email: req.body.lead_email
   }).then(lead => {
-    res.redirect('/');
+    res.redirect('/leads');
+  })
+});
+
+exports.show_leads = ((req, res, next) => {
+  models.Lead.findAll().then(leads => {
+      res.render('landing', { title: 'Express', leads: leads });
   })
 });
