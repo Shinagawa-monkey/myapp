fbb6e45bf3a72d1edf65e972825edacfab5e5046
Shinagawa-monkey <helena.shatalova.art@gmail.com>
2022-05-15 23:13:03 -0500

    Implement local auth strategy using email and password fields

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const passport = require('passport');
+const session = require('express-session');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+
+require('./passport_setup')(passport);
 
 const app = express();
 const port = 3000;
@@ -19,6 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({ secret: 'our new secret' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
